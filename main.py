69e36a6e5ee9f69c6585a47f2e00eee41c441510
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-05-18 22:20:35 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class Person:
         return self.name
 
 
-# Student class inheriting from Personxcxccxcvxvxvxvxvxvxvxvxzvxva ddfa
+# Student class inheriting from Personxcxccxcvxvxvxvxvxvxvxvxzvxva ddfasf
 class Student(Person):
     def __init__(self, name, age, roll_number):
         super().__init__(name, age)
